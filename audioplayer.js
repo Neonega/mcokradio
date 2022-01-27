6dfb261db1e3a684f7da140ea3d5002e643f0d0c
Neonega <95849002+Neonega@users.noreply.github.com>
2022-01-27 23:47:50 +0000

    Add files via upload

--- audioplayer.js
+++ audioplayer.js
@@ -609,3 +609,28 @@ exports.default = _default;
 
 },{}]},{},[1])(1)
 });
+
+
+window.onload = function(){
+
+  var myAudio = document.getElementById('ready-player-1');
+
+  myAudio.addEventListener('progress', function() {
+    var duration =  myAudio.duration;
+    if (duration > 0) {
+      for (var i = 0; i < myAudio.buffered.length; i++) {
+            if (myAudio.buffered.start(myAudio.buffered.length - 1 - i) < myAudio.currentTime) {
+                document.getElementById("buffered-amount").style.width = (myAudio.buffered.end(myAudio.buffered.length - 1 - i) / duration) * 100 + "%";
+                break;
+            }
+        }
+    }
+  });
+
+  myAudio.addEventListener('timeupdate', function() {
+    var duration =  myAudio.duration;
+    if (duration > 0) {
+      document.getElementById('progress-amount').style.width = ((myAudio.currentTime / duration)*100) + "%";
+    }
+  });
+}
